6c745f2af4471d53a531fc81c64ae7b45b8b6ad0
sleep <86079772+the-sleep@users.noreply.github.com>
2023-07-14 14:42:13 -0400

    Update index.php

--- upload/index.php
+++ upload/index.php
@@ -5,7 +5,6 @@ $target_file = $target_dir . basename($_FILES["imgUpload"]["name"]);
 $uploadOk = 1;
 $imageFileType = strtolower(pathinfo($target_file,PATHINFO_EXTENSION));
 
-// Check if image file is a actual image or fake image
 if(isset($_POST["submit"])) {
   $check = getimagesize($_FILES["imgUpload"]["tmp_name"]);
   if($check !== false) {
@@ -17,29 +16,24 @@ if(isset($_POST["submit"])) {
   }
 }
 
-// Check if file already exists
 if (file_exists($target_file)) {
   echo "Sorry, file already exists.";
   $uploadOk = 0;
 }
 
-// Check file size
 if ($_FILES["imgUpload"]["size"] > 500000) {
   echo "Sorry, your file is too large.";
   $uploadOk = 0;
 }
 
-// Allow certain file formats
 if($imageFileType != "jpg" && $imageFileType != "png" && $imageFileType != "jpeg"
 && $imageFileType != "gif" ) {
   echo "Sorry, only JPG, JPEG, PNG & GIF files are allowed.";
   $uploadOk = 0;
 }
 
-// Check if $uploadOk is set to 0 by an error
 if ($uploadOk == 0) {
   echo "Sorry, your file was not uploaded.";
-// if everything is ok, try to upload file
 } else {
   if (move_uploaded_file($_FILES["imgUpload"]["tmp_name"], $target_file)) {
     echo "The file ". htmlspecialchars( basename( $_FILES["imgUpload"]["name"])). " has been uploaded.";
