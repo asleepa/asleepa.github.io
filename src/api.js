730cf9e4190fc4af1a5caa7181be239d1e62d866
asleepa <86079772+asleepa@users.noreply.github.com>
2023-08-09 23:45:14 -0400

    Update api.js

--- src/api.js
+++ src/api.js
@@ -1,4 +1,4 @@
-require("dotenv").config();
+// require("dotenv").config();
 const queryString = window.location.search;
 const urlParams = new URLSearchParams(queryString);
 const apiType = urlParams.get("api_type")
